9d87840f6df3a1aa143ad345df65c0092f785052
Afohtim <afohmouth@gmail.com>
2019-07-09 22:52:45 +0300

    added characterInfo class

---
+++ Game/Assets/Scripts/CharacterInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    class CharacterInfo
+    {
+        public enum CharacterClass { Warrior, Mage, Archer, Rogue};
+
+        public Inventory inventory;
+        public Slots stots;
+        public float healthPoints;
+        public float manaPoints;
+        public float agilityPoints;
+        public float staminaPoints;
+        public Defence defence;
+        public MainInfo info;
+        public CharacterClass characterClass;
+    }
+}
