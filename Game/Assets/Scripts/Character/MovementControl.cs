e049055f1bdadba183d5e8f87d603d1053ce7875
Afohtim <afohmouth@gmail.com>
2019-08-10 05:37:53 +0300

    movement script changes

--- Game/Assets/Scripts/Character/MovementControl.cs
+++ Game/Assets/Scripts/Character/MovementControl.cs
@@ -7,63 +7,68 @@ public class MovementControl : MonoBehaviour
 
     public float defaultSpeed;
     public float defaultUpForce;
-    public int facingRight;
+    public bool  facingRight;
+    public float groundCheckRadius;
 
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+
+    private float moveInput;
     private int jumpCount;
-    private Animator animator;
+    private bool isGrounded;
+
+    //private Animator animator;
     private Rigidbody2D rb2d;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        facingRight = 1;
-        animator = GetComponent<Animator>();
+        facingRight = true;
+        //animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         jumpCount = 0;
         rb2d.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        float horizontalSpeed = 0;
-        if (jumpCount == 0 && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)))
-        {
-            rb2d.AddForce(new Vector2(0, defaultUpForce));
-            jumpCount++;
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            horizontalSpeed -= defaultSpeed;
-        }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            horizontalSpeed += defaultSpeed;
-        }
-        if (horizontalSpeed < 0 && facingRight == 1)
-        {
-            facingRight = 0;
-            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        }
-        if (horizontalSpeed > 0 && facingRight == 0)
-        {
-            facingRight = 1;
-            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        }
-
-        rb2d.velocity = new Vector2(horizontalSpeed, rb2d.velocity.y);
-
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "floor")
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        if(isGrounded)
         {
             jumpCount = 0;
         }
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(defaultSpeed * moveInput, rb2d.velocity.y);
+
+        if(facingRight && moveInput < 0 || !facingRight && moveInput > 0)
+        {
+            Flip();
+        }
+
+    }
+
+    private void Update()
+    {
+        if (jumpCount == 0 && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)))
+        {
+            rb2d.velocity = Vector2.up * defaultUpForce;
+            jumpCount++;
+        }
+        
+        //if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){}
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
